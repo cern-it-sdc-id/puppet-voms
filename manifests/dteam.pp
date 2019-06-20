0b9453e66ec9f4113ca0d4cf1b559a2b10f6f08b
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2019-06-20 15:31:54 +0900

    update voms2.hellasgrid.gr

--- manifests/dteam.pp
+++ manifests/dteam.pp
@@ -2,7 +2,7 @@ class voms::dteam {
   voms::client{'dteam':
       servers => [{ server                   => 'voms2.hellasgrid.gr',
                     port  => '15004',
-                    dn    => '/C=GR/O=HellasGrid/OU=hellasgrid.gr/CN=voms2.hellasgrid.gr',
+                    dn    => '/C=GR/O=HellasGrid/OU=athena.hellasgrid.gr/CN=voms2.hellasgrid.gr',
                     ca_dn => '/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2016'
                  }]
   }
--- manifests/see.pp
+++ manifests/see.pp
@@ -22,7 +22,7 @@ class voms::see {
                   },
                   { server                   => 'voms2.hellasgrid.gr',
                     port  => '15004',
-                    dn    => '/C=GR/O=HellasGrid/OU=hellasgrid.gr/CN=voms2.hellasgrid.gr',
+                    dn    => '/C=GR/O=HellasGrid/OU=athena.hellasgrid.gr/CN=voms2.hellasgrid.gr',
                     ca_dn => '/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2006'
                  }]
   }
